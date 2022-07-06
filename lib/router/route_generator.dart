4618a436367266d676f13f9556717fd4b8a611f9
merk <leonardo.mercado193@tectijuana.edu.mx>
2022-07-06 12:45:53 -0700

    upto chapter 21

--- lib/router/route_generator.dart
+++ lib/router/route_generator.dart
@@ -1,31 +1,31 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web/ui/pages/counter_page.dart';
 import 'package:flutter_web/ui/pages/counter_provider.dart';
 import 'package:flutter_web/ui/pages/page_404.dart';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 class RouteGenerator {
-  static Route<dynamic> generateRoute (RouteSettings settings){
-    switch(settings.name){
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case '/stateful':
-      
-        return MaterialPageRoute(
-          settings: RouteSettings(name: '/stateful'),
-          builder: (_) => CounterPage()
-          );
-        
-        case '/provider':
-          return  MaterialPageRoute(
-          settings: RouteSettings(name: '/provider'),
-          builder: (_) => CounterProviderPage()
-          );
+        return _fadeRoute(CounterPage(), '/stateful');
 
-          default:
-           return MaterialPageRoute(
-          settings: RouteSettings(name: '/404'),
-          builder: (_) => Page404()
-          );
+      case '/provider':
+        return _fadeRoute(CounterProviderPage(), '/provider');
 
+      default:
+        return _fadeRoute(Page404(), '/404');
     }
+  }
+
+  static PageRoute _fadeRoute(Widget child, String RouteName) {
+    return PageRouteBuilder(
+        settings: RouteSettings(name: RouteName),
+        pageBuilder: (_, __, ___) => child,
+        transitionsBuilder: (_, animation, __, ___) => 
+        
+        (kIsWeb) ? child : CupertinoPageTransition(primaryRouteAnimation: animation, secondaryRouteAnimation: animation, child: child, linearTransition: true),
+       
+    );
   }
 }
